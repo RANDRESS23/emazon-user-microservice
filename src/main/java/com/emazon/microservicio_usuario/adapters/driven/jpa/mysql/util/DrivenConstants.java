5cb33f5d3c29b7a8007921da2de9d89dda45c886
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-06 00:26:27 -0500

    feat: add driven constants

---
+++ src/main/java/com/emazon/microservicio_usuario/adapters/driven/jpa/mysql/util/DrivenConstants.java
@@ -0,0 +1,41 @@
+package com.emazon.microservicio_usuario.adapters.driven.jpa.mysql.util;
+
+public class DrivenConstants {
+    private DrivenConstants() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String DOCUMENT_REGEX = "^\\d+$";
+    public static final String PHONE_REGEX = "^\\+?\\d{1,13}$";
+    public static final String EMAIL_REGEX = "^[\\w!#$%&'*+/=?`{|}~^.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$";
+    public static final String BIRTHDATE_REGEX = "^\\d{2}/\\d{2}/\\d{4}$";
+
+    public static final int MAXIMUM_USER_NAME_CHARACTERS = 50;
+    public static final int MAXIMUM_USER_LASTNAME_CHARACTERS = 50;
+    public static final int MAXIMUM_USER_PHONE_CHARACTERS = 13;
+    public static final int MAXIMUM_ROLE_DESCRIPTION_CHARACTERS = 100;
+
+    public static final String USERS_TABLE_NAME = "user";
+    public static final String COLUMN_USER_ID = "user_id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_USER_LASTNAME = "lastname";
+    public static final String COLUMN_USER_DOCUMENT = "document";
+    public static final String COLUMN_USER_PHONE = "phone";
+    public static final String COLUMN_USER_EMAIL = "email";
+    public static final String COLUMN_USER_PASSWORD = "password";
+    public static final String COLUMN_USER_BIRTHDATE = "birthdate";
+    public static final String COLUMN_ROLE_ID = "role_id";
+
+    public static final String ROLES_TABLE_NAME = "role";
+    public static final String COLUMN_ROLE_DESCRIPTION = "description";
+
+    public static final String DOCUMENT_ALREADY_EXISTS_MESSAGE = "Document already exists.";
+    public static final String EMAIL_ALREADY_EXISTS_MESSAGE = "Email already exists.";
+    public static final String PHONE_ALREADY_EXISTS_MESSAGE = "Phone already exists.";
+    public static final String ROLE_ALREADY_EXISTS_MESSAGE = "Role already exists.";
+
+    public static final String INVALID_DOCUMENT = "The 'document' field is not valid";
+    public static final String INVALID_PHONE = "The 'phone' field is not valid";
+    public static final String INVALID_EMAIL = "The 'email' field is not valid";
+    public static final String INVALID_BIRTHDATE = "The 'birthdate' field is not valid";
+}
