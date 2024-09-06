5d72b9b64d437ddf0c2f76bec826d21840eefddc
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-06 00:24:23 -0500

    feat: add driving constants

---
+++ src/main/java/com/emazon/microservicio_usuario/adapters/driving/util/DrivingConstants.java
@@ -0,0 +1,14 @@
+package com.emazon.microservicio_usuario.adapters.driving.util;
+
+public class DrivingConstants {
+    private DrivingConstants() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String DATE_FORMAT = "dd/MM/yyyy";
+    public static final String USER_ID = "userId";
+    public static final String ROLE_ID = "roleId";
+    public static final String USER_BIRTHDATE = "birthdate";
+    public static final String FORMAT_FROM_STRING_TO_LOCAL_DATE = "formatFromStringToLocalDate";
+    public static final String AUX_BODEGA_USER_ROLE = "aux_bodega";
+}
