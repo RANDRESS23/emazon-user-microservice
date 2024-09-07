89cb9de152620e268846b8c55ab1d63640320c93
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-07 13:20:08 -0500

    feat: add Builder annotation to add user request dto

--- src/main/java/com/emazon/microservicio_usuario/adapters/driving/dto/request/AddUserRequest.java
+++ src/main/java/com/emazon/microservicio_usuario/adapters/driving/dto/request/AddUserRequest.java
@@ -1,10 +1,12 @@
 package com.emazon.microservicio_usuario.adapters.driving.dto.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@Builder
 public class AddUserRequest {
     private final String name;
     private final String lastName;
