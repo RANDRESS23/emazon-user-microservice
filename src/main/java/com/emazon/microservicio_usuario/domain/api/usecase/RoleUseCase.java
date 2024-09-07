790b65c5dd7634f21ea62e5b7b17c8c307e505e4
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-07 13:12:20 -0500

    refactor: remove save role method in the role use case

--- src/main/java/com/emazon/microservicio_usuario/domain/api/usecase/RoleUseCase.java
+++ src/main/java/com/emazon/microservicio_usuario/domain/api/usecase/RoleUseCase.java
@@ -1,34 +1,21 @@
 package com.emazon.microservicio_usuario.domain.api.usecase;
 
 import com.emazon.microservicio_usuario.domain.api.IRoleServicePort;
-import com.emazon.microservicio_usuario.domain.exception.AlreadyExistsFieldException;
+import com.emazon.microservicio_usuario.domain.enums.RoleEnum;
 import com.emazon.microservicio_usuario.domain.exception.NotFoundException;
 import com.emazon.microservicio_usuario.domain.model.Role;
 import com.emazon.microservicio_usuario.domain.spi.IRolePersistencePort;
 import com.emazon.microservicio_usuario.domain.util.DomainConstants;
-import com.emazon.microservicio_usuario.domain.validation.RoleValidation;
 
 public class RoleUseCase implements IRoleServicePort {
     private final IRolePersistencePort rolePersistencePort;
-    private final RoleValidation roleValidation;
 
-    public RoleUseCase(IRolePersistencePort rolePersistencePort, RoleValidation roleValidation) {
+    public RoleUseCase(IRolePersistencePort rolePersistencePort) {
         this.rolePersistencePort = rolePersistencePort;
-        this.roleValidation = roleValidation;
     }
 
     @Override
-    public void saveRole(Role role) {
-        if (rolePersistencePort.getRoleByName(role.getName()).isPresent()) {
-            throw new AlreadyExistsFieldException(DomainConstants.ROLE_ALREADY_EXISTS_MESSAGE);
-        }
-
-        roleValidation.validateRole(role);
-        rolePersistencePort.saveRole(role);
-    }
-
-    @Override
-    public Role getRole(String name) {
+    public Role getRole(RoleEnum name) {
         return rolePersistencePort.getRoleByName(name)
                 .orElseThrow(() -> new NotFoundException(DomainConstants.ROLE_NOT_FOUND));
     }
