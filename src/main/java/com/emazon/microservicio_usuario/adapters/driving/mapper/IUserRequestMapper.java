a867b91485109e6ee0c4d5df403bc32e17410736
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-07 13:23:21 -0500

    refactor: fix user request mapper addRequestToUser mapper

--- src/main/java/com/emazon/microservicio_usuario/adapters/driving/mapper/IUserRequestMapper.java
+++ src/main/java/com/emazon/microservicio_usuario/adapters/driving/mapper/IUserRequestMapper.java
@@ -14,7 +14,19 @@ import java.time.format.DateTimeFormatter;
 public interface IUserRequestMapper {
     @Mapping(target = DrivingConstants.USER_ID, ignore = true)
     @Mapping(source = DrivingConstants.USER_BIRTHDATE, target = DrivingConstants.USER_BIRTHDATE, qualifiedByName = DrivingConstants.FORMAT_FROM_STRING_TO_LOCAL_DATE)
-    User addRequestToUser(AddUserRequest addUserRequest);
+    public static User addRequestToUser(AddUserRequest addUserRequest) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DrivingConstants.DATE_FORMAT);
+
+        return new User.UserBuilder()
+                .name(addUserRequest.getName())
+                .lastName(addUserRequest.getLastName())
+                .document(addUserRequest.getDocument())
+                .phone(addUserRequest.getPhone())
+                .birthdate(LocalDate.parse(addUserRequest.getBirthdate(), formatter))
+                .email(addUserRequest.getEmail())
+                .password(addUserRequest.getPassword())
+        .build();
+    }
 
     @Named(DrivingConstants.FORMAT_FROM_STRING_TO_LOCAL_DATE)
     default LocalDate formatFromStringToLocalDate(String date) {
