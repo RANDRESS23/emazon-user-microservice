1f22ff8b1964776bb8ee7ed6fac4650142d2046f
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-07 13:14:56 -0500

    refactor: remove save role method in the role adapter

--- src/main/java/com/emazon/microservicio_usuario/adapters/driven/jpa/mysql/adapter/RoleAdapter.java
+++ src/main/java/com/emazon/microservicio_usuario/adapters/driven/jpa/mysql/adapter/RoleAdapter.java
@@ -1,9 +1,8 @@
 package com.emazon.microservicio_usuario.adapters.driven.jpa.mysql.adapter;
 
-import com.emazon.microservicio_usuario.adapters.driven.jpa.mysql.exception.AlreadyExistsException;
 import com.emazon.microservicio_usuario.adapters.driven.jpa.mysql.mapper.IRoleEntityMapper;
 import com.emazon.microservicio_usuario.adapters.driven.jpa.mysql.repository.IRoleRepository;
-import com.emazon.microservicio_usuario.adapters.driven.jpa.mysql.util.DrivenConstants;
+import com.emazon.microservicio_usuario.domain.enums.RoleEnum;
 import com.emazon.microservicio_usuario.domain.model.Role;
 import com.emazon.microservicio_usuario.domain.spi.IRolePersistencePort;
 import lombok.RequiredArgsConstructor;
@@ -16,16 +15,7 @@ public class RoleAdapter implements IRolePersistencePort {
     private final IRoleEntityMapper roleEntityMapper;
 
     @Override
-    public void saveRole(Role role) {
-        if (roleRepository.findByName(role.getName()).isPresent()) {
-            throw new AlreadyExistsException(DrivenConstants.ROLE_ALREADY_EXISTS_MESSAGE);
-        }
-
-        roleRepository.save(roleEntityMapper.toEntity(role));
-    }
-
-    @Override
-    public Optional<Role> getRoleByName(String name) {
+    public Optional<Role> getRoleByName(RoleEnum name) {
         return roleRepository.findByName(name)
                 .map(roleEntityMapper::toDomainModel);
     }
