b6f1cf95dbe8c75349346386a521e30db795c38b
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-10 10:54:38 -0500

    feat: implement security to the /aux-bodega-user endpoint

--- src/main/java/com/emazon/microservicio_usuario/adapters/driving/controller/UserRestController.java
+++ src/main/java/com/emazon/microservicio_usuario/adapters/driving/controller/UserRestController.java
@@ -4,6 +4,7 @@ import com.emazon.microservicio_usuario.adapters.driving.dto.request.AddUserRequ
 import com.emazon.microservicio_usuario.adapters.driving.dto.response.UserResponse;
 import com.emazon.microservicio_usuario.adapters.driving.mapper.IUserRequestMapper;
 import com.emazon.microservicio_usuario.adapters.driving.mapper.IUserResponseMapper;
+import com.emazon.microservicio_usuario.adapters.driving.util.DrivingConstants;
 import com.emazon.microservicio_usuario.domain.api.IRoleServicePort;
 import com.emazon.microservicio_usuario.domain.api.IUserServicePort;
 import com.emazon.microservicio_usuario.domain.enums.RoleEnum;
@@ -13,6 +14,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +29,7 @@ public class UserRestController {
     private final IRoleServicePort roleServicePort;
 
     @PostMapping("/aux-bodega-user")
+    @PreAuthorize(DrivingConstants.HAS_ROLE_ADMIN)
     public ResponseEntity<UserResponse> addAuxBodegaUser(@Valid @RequestBody AddUserRequest request) {
         User user = IUserRequestMapper.addRequestToUser(request);
         Role role = roleServicePort.getRole(RoleEnum.AUX_BODEGA);
