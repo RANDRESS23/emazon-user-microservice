18f81188cee72c6f4b9fcc548d9d96a2dab685c6
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-12 10:26:05 -0500

    refactor: remove toEntity method from the role entity mapper

--- src/main/java/com/emazon/microservicio_usuario/adapters/driven/jpa/mysql/mapper/IRoleEntityMapper.java
+++ src/main/java/com/emazon/microservicio_usuario/adapters/driven/jpa/mysql/mapper/IRoleEntityMapper.java
@@ -6,7 +6,5 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface IRoleEntityMapper {
-    RoleEntity toEntity(Role role);
-
     Role toDomainModel(RoleEntity roleEntity);
 }
