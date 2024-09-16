fc9ebc699f2b10662fbe067862756a42dd0fdb64
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-16 00:09:30 -0500

    feat: add save client method in the user use case

--- src/main/java/com/emazon/microservicio_usuario/domain/api/usecase/UserUseCase.java
+++ src/main/java/com/emazon/microservicio_usuario/domain/api/usecase/UserUseCase.java
@@ -1,7 +1,10 @@
 package com.emazon.microservicio_usuario.domain.api.usecase;
 
+import com.emazon.microservicio_usuario.domain.api.IRoleServicePort;
 import com.emazon.microservicio_usuario.domain.api.IUserServicePort;
+import com.emazon.microservicio_usuario.domain.enums.RoleEnum;
 import com.emazon.microservicio_usuario.domain.exception.AlreadyExistsFieldException;
+import com.emazon.microservicio_usuario.domain.model.Role;
 import com.emazon.microservicio_usuario.domain.model.User;
 import com.emazon.microservicio_usuario.domain.spi.IEncryptionPersistencePort;
 import com.emazon.microservicio_usuario.domain.spi.IUserPersistencePort;
@@ -10,17 +13,19 @@ import com.emazon.microservicio_usuario.domain.validation.UserValidation;
 
 public class UserUseCase implements IUserServicePort {
     private final IUserPersistencePort userPersistencePort;
+    private final IRoleServicePort roleServicePort;
     private final IEncryptionPersistencePort encryptionPersistencePort;
     private final UserValidation userValidation;
 
-    public UserUseCase(IUserPersistencePort userPersistencePort, IEncryptionPersistencePort encryptionPersistencePort, UserValidation userValidation) {
+    public UserUseCase(IUserPersistencePort userPersistencePort, IRoleServicePort roleServicePort, IEncryptionPersistencePort encryptionPersistencePort, UserValidation userValidation) {
         this.userPersistencePort = userPersistencePort;
+        this.roleServicePort = roleServicePort;
         this.encryptionPersistencePort = encryptionPersistencePort;
         this.userValidation = userValidation;
     }
 
     @Override
-    public void saveAuxBodegaUser(User user) {
+    public String validateUser(User user) {
         if (userPersistencePort.getUserByDocument(user.getDocument()).isPresent()) {
             throw new AlreadyExistsFieldException(DomainConstants.DOCUMENT_ALREADY_EXISTS_MESSAGE);
         }
@@ -34,8 +39,27 @@ public class UserUseCase implements IUserServicePort {
         }
 
         userValidation.validateUser(user);
-        String encryptedPassword = encryptionPersistencePort.encodePassword(user.getPassword());
+
+        return encryptionPersistencePort.encodePassword(user.getPassword());
+    }
+
+    @Override
+    public User saveAuxBodegaUser(User user) {
+        String encryptedPassword = validateUser(user);
+        Role role = roleServicePort.getRole(RoleEnum.AUX_BODEGA);
+        user.setRole(role);
         user.setPassword(encryptedPassword);
-        userPersistencePort.saveUser(user);
+
+        return userPersistencePort.saveUser(user);
+    }
+
+    @Override
+    public User saveClient(User user) {
+        String encryptedPassword = validateUser(user);
+        Role role = roleServicePort.getRole(RoleEnum.CLIENTE);
+        user.setRole(role);
+        user.setPassword(encryptedPassword);
+
+        return userPersistencePort.saveUser(user);
     }
 }
