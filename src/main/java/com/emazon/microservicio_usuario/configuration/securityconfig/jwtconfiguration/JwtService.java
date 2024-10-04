4a5100304f769f5c70008c7be1a621dd66ac6cb4
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-10-04 15:18:15 -0500

    refactor: change token expiration time to one week

--- src/main/java/com/emazon/microservicio_usuario/configuration/securityconfig/jwtconfiguration/JwtService.java
+++ src/main/java/com/emazon/microservicio_usuario/configuration/securityconfig/jwtconfiguration/JwtService.java
@@ -32,7 +32,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(user.getEmail())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24 * 7))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
